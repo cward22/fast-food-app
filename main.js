fbc415d7f547aef98b453da5d9f84a6a5ed8b3f1
New Life IT <h2hworldwide@gmail.com>
2020-03-18 16:05:28 -0400

    added comments

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
+// Create an object named menu where we'll save the menu for the restaurant. For each property (Each property should be a string like "fries") and add a number as the value.
+
 var menu = {
     "burgers": 100,
-    "shakes": 100,
-    "fries": 100
+    "shakes": 150,
+    "fries": 200
     // "boneless buffalo wings": 300,
     // "veggie burger": 100,
     // "sweet potato fries": 200
 }
-   
+
+// take the customer's order and save it to a variable named customerOrder
+
 // attach an event listener to the order button and listen for the click event
 document.getElementById("orderButton").addEventListener("click", function() {
     // take the customer's order and save it to a variable named customerOrder
@@ -26,4 +30,10 @@ document.getElementById("orderButton").addEventListener("click", function() {
 document.getElementById("userInput").addEventListener("click", function() {
     document.getElementById("output").innerHTML = "";
 });
+
+// Take the customerOrder variable, (which should be a string) and split it on the comma character. 
+
+// var customerOrder = document.getElementById("userInput").value;
+// customerOrder.split(",");
+
 
