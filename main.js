5a2a973119dc03f4e8c97076a48f225de04abb87
New Life IT <h2hworldwide@gmail.com>
2020-03-23 00:50:54 -0400

    attempted Exercise 3

--- main.js
+++ main.js
@@ -3,27 +3,20 @@ var menu = {
     "burgers": 100,
     "shakes": 150,
     "fries": 200
-    // "boneless buffalo wings": 300,
-    // "veggie burger": 100,
-    // "sweet potato fries": 200
 }
-
-// document.getElementById("inventoryButton").addEventListener("click", function() {
-//     document.getElementById("output2").innerHTML = menu[0];
-// });
 
 // Attach an event listener to the order button and listen for the click event.
 let orderButton = document.getElementById("orderButton").addEventListener("click", () => {
     // Take the customer's order and save it to a variable named customerOrder.
-        let customerOrder = document.getElementById("userInput").value;
-        let order = customerOrder
+    let customerOrder = document.getElementById("userInput").value;
+    let order = customerOrder
 
     // Take the customerOrder variable, (which should be a string) and split it on the comma character and trim white space after each comma. 
-        .split(",")
-        .map(x => x.trim())
-
-        console.log(customerOrder);
-        console.log(order);
+    .split(",")
+    .map(x => x.trim())
+    // Log the customer's order to the console.
+    console.log(customerOrder);
+    console.log(order);
 
     // Loop through the entire order array (from 0 to the length of the array). In the loop split each item of this array on the colon character. Trim the leading and trailing whitespace again.
 
@@ -31,20 +24,64 @@ let orderButton = document.getElementById("orderButton").addEventListener("click
         order[i] = order[i].split(":").map(item => 
             item.trim());
             console.log(order[i]);
-        }
+    }
         
-    // Make a string with the customer's order and repeat order back to customer.
-    document.getElementById("output").innerHTML = `Your order -  ${customerOrder} - is being prepared!`;
-     // Log the customer's order to the console.
+    for (var key in menu) {
 
-})
+        if(customerOrder == key) {
+        console.log("order can be fulfilled");
+        } else {
+        console.log("item not in stock");
+        
+        // if(order[1] < menu[1]) {
+        // console.log("order can be fulfilled")
+        // } else {
+        // console.log("order cannot be fulfilled");
+        // }
+     }
+    //     if(order[i] == menu[0]) {
+    //     console.log([key] + " is on menu");
+    //     } else {
+    //         console.log("item not on menu");
+    //     };
+    // };
+        // } else if (order[1] < menu[1]) {
+   
+    // for (var name in menu) {
+    //     if(order[0] === menu[0]) {
+    //     console.log([name] + " is on menu");
+        // } else if (order[1] < menu[1]) {
+        // console.log("still in stock");
+        // } else {
+        //     // document.getElementById("output2").innerHTML = "cannot currently fulfill order";
+        // console.log("not in stock");
+        }
+    
+// Make a string with the customer's order and repeat order back to customer.
+document.getElementById("output").innerHTML = `Your order -  ${customerOrder} - is being prepared!`;
 
 // Clear input field after each order submission.
 document.getElementById("orderButton").addEventListener("click", function() {
     document.getElementById("userInput").value = "";
-});
+    });
 
 // Clear output message when next customer clicks inside input field.
 document.getElementById("userInput").addEventListener("click", function() {
     document.getElementById("output").innerHTML = "";
-});
+    });
+
+})
+
+function sendRequest () {
+    var xhr = new XMLHttpRequest();
+    xhr.addEventListener("load", sendRequest);
+    xhr.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200 ) {
+            console.log(this.responseText)
+        } else {
+            console.log("Status Error");
+        }
+    };
+    xhr.open("GET", "https://swapi.co/documentation#root");
+    xhr.send();
+}
